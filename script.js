fca48703c94563bcf6919575612fb482ef3e46f2
Thiago Roberto dos Santos Oliveira <trpanco@hotmail.com>
2025-09-24 21:39:28 -0300

    feat: Adiciona Interatividade ao botão de gerar conselho

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const conselhos = {
+const conselhos = [
     "Acredite em Você mesmo." , "O primeiro passo é o mais importante" ,
     "A persistência realiza o impossível." , "Aprenda algo novo hoje" ,
     "Seja gentil com todos que encontrar."
+]
+const conselhoTexto = document.getElementById('conselhoTexto');
+const novoConselhoBtn = document.getElementById('novoconselhoBtn');
+
+function gerarConselho() {
+    //BUG INTENCIONAL AQUI
+    const indice = Math.floor(Math.random() * (conselhos.length + 1));
+    conselhoTexto.textContent = conselhos[indice];
 }
-console.log("Arquivo de script carregado. Array de conselhos pronto.");
+novoConselhoBtn.addEventListener('click' , gerarConselho);
