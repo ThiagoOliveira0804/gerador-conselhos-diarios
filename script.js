4893393a98f9c5d14a051fdaecda1563ec8d115f
Thiago Roberto dos Santos Oliveira <trpanco@hotmail.com>
2025-09-24 21:21:28 -0300

    Criação e Upload do README.md e dos demais arquivos

---
+++ script.js
@@ -0,0 +1,6 @@
+const conselhos = {
+    "Acredite em Você mesmo." , "O primeiro passo é o mais importante" ,
+    "A persistência realiza o impossível." , "Aprenda algo novo hoje" ,
+    "Seja gentil com todos que encontrar."
+}
+console.log("Arquivo de script carregado. Array de conselhos pronto.");
